245d31e0bc3715d298f768c1f1ac463dd2e5428c
Muralisanku <sankumurali4a6@gmail.com>
2025-03-31 18:56:37 +0530

    mssing

---
+++ examples/complete/main.tf
@@ -0,0 +1,20 @@
+module "roboshop" {
+  #source = "../terraform-aws-vpc"
+  source = "git::https://github.com/Muralisanku/terraform-aws-vpc.git?ref=main"
+  project_name = var.project_name
+  environment = var.environment
+  common_tags = var.common_tags
+  vpc_tags = var.vpc_tags
+
+# Public subnet
+public_subnets_cidr = var.public_subnets_cidr
+
+# Private subnet
+private_subnets_cidr = var.private_subnets_cidr
+
+# database subnet
+database_subnets_cidr = var.database_subnets_cidr
+
+is_peering_required = var.is_peering_required
+
+}
