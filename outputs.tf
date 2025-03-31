56f5d6a870b5a4840f6e7f458504f9ec39bd5d9e
Muralisanku <sankumurali4a6@gmail.com>
2025-03-31 18:37:40 +0530

    mssing

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
-# output "azs" {
-#   value = data.aws_availability_zones.azs.names
-  
-# }
+output "azs" {
+  value = data.aws_availability_zones.azs.names
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
